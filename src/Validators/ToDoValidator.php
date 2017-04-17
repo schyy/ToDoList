49b42aca82121ffe2a5d71b526b6adb7e8e78983
schyy <b.kosinski@azupedia.com>
2017-04-17 17:01:20 +0200

    start

---
+++ src/Validators/ToDoValidator.php
@@ -0,0 +1,18 @@
+
+
+    <?php
+
+    namespace ToDoList\Validators;
+
+    use Plenty\Validation\Validator;
+
+    /**
+     *  Validator Class
+     */
+    class ToDoValidator extends Validator
+    {
+        protected function defineAttributes()
+        {
+            $this->addString('taskDescription', true);
+        }
+    }
